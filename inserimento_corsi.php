c8cae80ab7d1f963598e0a7aed9dcc3b61af292d
IsThisDemi <furryalt68@gmail.com>
2023-09-08 17:49:45 +0200

    struttura visual inserimento corsi

--- inserimento_corsi.php
+++ inserimento_corsi.php
@@ -56,129 +56,37 @@ else
             }
         }
     }
-?>
-<!DOCTYPE html>
-<html lang="it">
-<head>
-    <title>Login FitnessCenter</title>
-    <meta charset="utf-8"/>
-    <meta name="keywords" content="Login FitnessCenter"/>
-    <meta name="description" content="pagina di login per FitnessCenter" />
-	<meta name="author" content=""/>
-    <meta name="viewport" content="width=device-width, initial-scale=1"/>
-    <link type="text/css"  rel="stylesheet" href="css/style.css" />
-	<link rel="stylesheet" media="print" href="css/print.css" type="text/css"/>
-	<link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon"/>
-</head>
-<body>
+    //Includo file html
+    $paginaHTML = file_get_contents("html/inserimento_corsi.html");
+    $formHTML = file_get_contents("html/inserimento_form.html");
+    //Includo footer
+    $footer = file_get_contents("utilityphp/footer.php");
+    //Gererazione header
+    $header = genera_header("inserimento corsi");
 
-<?php 
-
-    echo genera_header("inserimento corsi");
     $categorie;
     if(GetCategorie($categorie)!="success"||!$categorie){
-
-        ?>
-
-            <p>errore connessione o database</p>
-        </body>
-        
-        <?php 
+        $contenuto = '<p>errore connessione o database</p>';
     }
     else{
-
-?>
-<div id="content" class="adminpage">
-<?php
-if($DisplayMessage!=""){
-        echo '<p class="formresult">';
-        echo $DisplayMessage;
-        echo '</p>';
-    }
-?>
-    <form class="adminform" action="inserimento_corsi.php?action=insert" method="post" id="inserisci">
-        <fieldset>
-        <label for="nome_corso" lang="it">Nome</label>    
-        <input id="nome_corso" type="text" placeholder="Nome corso" name="nome_corso" required />
-        <label for="categoria_corso" lang="it">Categoria</label>
-        <select id="categoria_corso" name="categoria_corso" required>
-<?php   
+        if($DisplayMessage!=""){
+            $DisplayMessage='<p class="formresult">'.$DisplayMessage.'</p>';
+        }
         $categorie;
+        $lista_categorie = '';
         GetCategorie($categorie);
         foreach($categorie as $id=>$nome){
-            echo "<option value=".$id.">".$nome."</option>";
+            $lista_categorie .= "<option value=".$id.">".$nome."</option>";
         }
-?>
-        </select>
-        </fieldset>
-        <fieldset>
-            <legend>Presentazione:</legend>
-            <label for="descrizione_corso" lang="it">Descrizione</label>    
-            <textarea id="descrizione_corso" name="descrizione_corso" rows="4" cols="50" required>
-                Breve descrizione del corso e delle attività svolte
-            </textarea>
-            <label for="link_immagine" lang="it">Link immagine</label>    
-            <input id="link_immagine" type="text" placeholder="image.png" name="link_immagine" required />
-            <label for="alt_immagine" lang="it">Alt text immagine</label>    
-            <textarea id="alt_immagine" name="alt_immagine" rows="4" cols="25" required>
-                Breve descrizione dell' immagine
-            </textarea>
-        </fieldset>
-        <fieldset>
-            <legend>Caratterisstice</legend>
-                <label for="forza"> forza</label>
-                <input type="checkbox" id="forza" name="forza" value="forza">
-                <label for="equilibrio"> equilibrio</label>
-                <input type="checkbox" id="equilibrio" name="equilibrio" value="equilibrio">
-                <label for="resistenza">resistenza</label>
-                <input type="checkbox" id="resistenza" name="resistenza" value="resistenza">
-                <label for="stabilità">stabilità</label>
-                <input type="checkbox" id="stabilità" name="stabilità" value="stabilità">
-                <label for="intensita">intensita</label>
-                <input type="number" id="intensita" name="intensita" required min="0" max="3">
-
-                <label for="durata">durata in minuti</label>
-                <!--<input type="number" id="durata" name="durata" required>-->
-                <input type="text" id="durata" name="durata" placeholder="30-45" required>
-                <label for="calorie">calorie esercizio</label>
-                <input type="number" id="calorie" name="calorie" required>
-        </fieldset>
-        <fieldset>
-            <legend>equipaggiamento</legend>
-            <label for="calzature" lang="it">calzature</label>
-            <select id="calzature" name="equipaggiamento_calzature" required>
-                <option value="1">calzini</option>
-                <option value="2">scarpe_sportive</option>
-                <option value="3">scarpe outdoor</option>
-                <option value="4">ciabatte</option>
-                <option value="5">piedi nudi</option>
-            </select>
-                <label for="asciugamano"> asciugamano</label>
-                <input type="checkbox" id="asciugamano" name="asciugamano" value="asciugamano">
-                <label for="borraccia"> borraccia</label>
-                <input type="checkbox" id="borraccia" name="borraccia" value="borraccia">
-                <label for="tappetino">tappetino</label>
-                <input type="checkbox" id="tappetino" name="tappetino" value="tappetino">
-                <label for="guantoni">guantoni</label>
-                <input type="checkbox" id="guantoni" name="guantoni" value="guantoni">
-                <label for="capelli_raccolti">capelli raccolti</label>
-                <input type="checkbox" id="capelli_raccolti" name="capelli_raccolti" value="capelli_raccolti">
-                <label for="abbigliamento_outdoor">abbigliamento_outdoor</label>
-                <input type="checkbox" id="abbigliamento_outdoor" name="abbigliamento_outdoor" value="abbigliamento_outdoor">
-                <label for="accappatoio">accappatoio</label>
-                <input type="checkbox" id="accappatoio" name="accappatoio" value="accappatoio">
-                <label for="cuffia">cuffia</label>
-                <input type="checkbox"id="cuffia" name="cuffia" value="cuffia">
-                <label for="costume">costume</label>
-                <input type="checkbox" id="costume" name="costume" value="costume">
-        </fieldset>
-        
-        <input type="submit" name="inserisci">
-    </form>
-</div>
-
-</body>
-<?php
+        $campi_replace_form = array("%messaggio%", "%categorie%");
+        $valori_replace_form = array($DisplayMessage, $lista_categorie);
+        $paginaHTML_form = str_replace($campi_replace_form, $valori_replace_form, $formHTML);
     }
+
+    //Sostituzione dei campi della pagina html con i valori				
+    $campi_replace = array("%header%", "%footer%", "%contenuto%");
+    $valori_replace = array($header, $footer, $paginaHTML_form);
+    $paginaHTML = str_replace($campi_replace, $valori_replace, $paginaHTML);
+    echo $paginaHTML;
 }
 ?>
